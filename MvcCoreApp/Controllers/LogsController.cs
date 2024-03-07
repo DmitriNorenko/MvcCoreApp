b60301236a8720dbff467e0ee8b903b1d880dd49
Name <email@exaple.ru>
2024-03-07 15:23:01 +0300

    32.11.2

---
+++ MvcCoreApp/Controllers/LogsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreApp.Models.Db;
+
+namespace MvcCoreApp.Controllers
+{
+    public class LogsController: Controller
+    {
+        public readonly IRequestRepository _request;
+        public readonly ILogger<LogsController> _logger;
+        public LogsController(IRequestRepository request, ILogger<LogsController> logger)
+        {
+            _request = request;
+            _logger = logger;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var requests = await _request.ShowLog();
+            return View(requests);
+        }
+    }
+}
